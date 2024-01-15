f53fff61e5348c3a7fbf48b7e6cb56cc7bdcc8c6
Unicorn <unicorn@wonderworld.168.1.16>
2024-01-15 15:46:10 +0300

    i2c

--- driver/main.c
+++ driver/main.c
@@ -6,7 +6,12 @@
 int main()
 {
     dr_init(SLAVE_ADDRESS);
-    short data = dr_get_reg_data(1);
-    printf("Here is data from 0x1: %d\n", data);
+    short data = dr_get_reg_data(2);
+    printf("Here is data from 0x2: %d\n", data);
+    data = CONFIG_MASK & (~(0ul));
+    dr_set_reg_data(2, data);
+    data = dr_get_reg_data(2);
+    printf("Here is data from 0x2: %d\n", data);
+    dr_exit();
     return 0;
 }
